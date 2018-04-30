810ac1108a936f2c9c6f94e94e1c8efe509810d0
kflorante <kflorante@DESKTOP-H69SVTV.localdomain>
2018-04-30 13:39:43 +0200

    feat: add a route to the MapContainer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import React, { Component } from 'react';
-import './App.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import MapContainer from './components/MapContainer.jsx';
 
 class App extends Component {
+
+  renderMapContainer = (user) => {
+    console.log('user: ', user.match.params.user)
+    console.log('tags: ',user.location.search.split(/[?+]/).splice(1))
+    return <MapContainer />;
+  }
+
   render() {
     return (
-      <MapContainer />
+      <Router>
+        <div>
+          <Route path="/barcelona/:user" render={this.renderMapContainer} />
+        </div>
+      </Router>
     );
+    // return (
+    //   <MapContainer />
+    // );
   }
 }
 
