f9efba63a3827a057017d33e8229d18a47716a16
kflorante <kflorante@DESKTOP-H69SVTV.localdomain>
2018-04-30 10:15:24 +0200

    feat: render multiple markers stored from the state

--- src/components/MapContainer.jsx
+++ src/components/MapContainer.jsx
@@ -1,58 +1,39 @@
 import React, { Component } from 'react';
-import { GoogleApiWrapper, Map, Marker, InfoWindow } from 'google-maps-react';
+import { GoogleApiWrapper, Map, Marker } from 'google-maps-react';
 
 export class MapContainer extends Component {
-  state = {
-    infoWindowVisibility : false,
-    activeMarker : {},
-    selectedPlace: {}
-  }
 
-  onMarkerClick = (props, marker, e) => {
-    this.setState({
-      selectedPlace: props,
-      activeMarker: marker,
-      infoWindowVisibility: true
-    })
-  }
-
-  onMapClicked = (props) => {
-    if (this.state.infoWindowVisibility) {
-      this.setState({
-        infoWindowVisibility: false,
-        activeMarker: null
-      })
-    }
+  state =  {
+    markers: [
+      {
+        place: 'Badalona',
+        position: {
+          lat: 41.446988,
+          lng: 2.245032
+        }
+      },
+      {
+        place: 'Barcelona Airport',
+        position: {
+          lat: 41.297445,
+          lng: 2.083294
+        }
+      },
+      {
+        place: 'El Papiol',
+        position: {
+          lat: 41.438722,
+          lng: 2.012628
+        }
+      }
+    ]
   }
 
   render() {
     return (
       <div>
-        <Map
-          google={this.props.google}
-          initialCenter={{
-            lat: 41.390205,
-            lng: 2.154007
-          }}
-          zoom={12}
-          onClick={this.onMapClicked}
-          onReady={this.fetchPlaces}
-        >
-          <Marker
-            onClick={this.onMarkerClick}
-            title={'Barcelona, Spain'}
-            name={'Barcelona, Spain'}
-            position={{lat: 41.390205, lng: 2.154007}}
-          />
-          <InfoWindow
-            marker = {this.state.activeMarker}
-            visible = {this.state.infoWindowVisibility}
-          >
-            <div>
-              <h2>{this.state.selectedPlace.name}</h2>
-              <img src="https://d15gqlu8dfiqiu.cloudfront.net/s3fs-public/styles/banner/public/images/chapters/Barcelona_travel_massive.jpg" alt="" width="300" height="250" />
-            </div>
-          </InfoWindow>
+        <Map google={this.props.google} initialCenter={{lat: 41.438722, lng: 2.012628}} zoom={12}>
+          {this.state.markers.map((marker) => <Marker position={marker.position} title={marker.place} key={Math.round(Math.random() * 10000 + 1)} />)}
         </Map>
       </div>
     );
